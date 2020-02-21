7364749a587c567eab31682b4516f24b38da5873
sunyoung5969 <61302754+sunyoung5969@users.noreply.github.com>
2020-02-21 14:52:19 +0900

    Update gittest333.java

--- src/gittest333.java
+++ src/gittest333.java
@@ -3,7 +3,7 @@ public class gittest333 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("이건 될까?");
+		System.out.println("이건 됐어요?");
 	}
 
 }
